a98261e555a6ea8a3f0dd22e4dd8cc17816bc604
Mariana Belo <101350371+MarianaBelo26@users.noreply.github.com>
2024-01-21 15:33:31 -0300

    Add Quiz 'Do you know?'

---
+++ quiz/script.js
@@ -0,0 +1,92 @@
+const form = document.querySelector('.quiz-form')
+const buttonInitQuiz = document.querySelector('.button-init-quiz')
+const initionPage = document.querySelector('.inition-page')
+const finalResult = document.querySelector('.final-score-container')
+
+
+const correctAnswers = ['A', 'D','B','A','D','C','A','B','A','C']
+
+let score = 0
+
+buttonInitQuiz.addEventListener('click', ()=>{
+    initionPage.classList.add('d-none')
+    form.classList.remove('d-none')
+})
+
+const getUserAnswers = () =>{
+	let userAnswers = []
+	
+	correctAnswers.forEach((_,index) =>{
+		const userAnswer = form[`inputQuestion${index + 1}`].value
+		userAnswers.push(userAnswer)
+	})
+	return userAnswers
+}
+
+const calculateUserScore = (userAnswers) =>{
+	userAnswers.forEach((userAnswer, index) =>{
+		const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+		
+		if(isUserAnswerCorrect){
+			score += 10
+		}
+	})
+}
+
+const showResultScore = ()=>{
+	scrollTo(0,0)
+	finalResult.classList.remove('d-none')
+}
+
+const animationResultScore = ()=>{
+	let counter = 0
+	const timer = setInterval(() =>{
+	   
+	if(counter === score){
+	   clearInterval(timer)
+	}
+	
+	finalResult.querySelector('span').textContent = `${counter++}%` 
+   }, 20)
+}
+
+const showRestartPopup = () =>{
+	const restartQuiz = setTimeout(() =>{
+		const popupWrapper = document.querySelector('.popup-wrapper') 
+		const restartPopup = document.querySelector('.container-restart-popup')
+		const yesButtonPopup = document.querySelector('.yes-button-popup')
+		const noButtonPopup = document.querySelector('.no-button-popup')
+		
+		restartPopup.classList.remove('d-none')
+		popupWrapper.style.display = 'block'
+
+			
+		yesButtonPopup.addEventListener('click', () =>{
+			restartPopup.classList.add('d-none')
+			finalResult.classList.add('d-none')
+			initionPage.classList.remove('d-none')
+			form.classList.add('d-none')
+			popupWrapper.style.display = 'none'
+		})
+			
+		noButtonPopup.addEventListener('click', () =>{
+			restartPopup.classList.add('d-none')
+			popupWrapper.style.display = 'none'
+		})
+	}, 2200)
+}
+
+
+form.addEventListener('submit', event =>{
+    event.preventDefault()
+	score = 0 
+	
+   const userAnswers = getUserAnswers()
+   
+   calculateUserScore(userAnswers)
+   showResultScore()
+   animationResultScore()
+   showRestartPopup()
+   
+})
+
